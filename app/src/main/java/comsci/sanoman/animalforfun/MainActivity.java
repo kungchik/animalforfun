49787717c0e5b0af06b7dd6d4e927a1f704e27ad
kungchik <non_ma_shi@hotmail.com>
2017-03-30 11:58:25 +0700

    Start Game Complete

--- app/src/main/java/comsci/sanoman/animalforfun/MainActivity.java
+++ app/src/main/java/comsci/sanoman/animalforfun/MainActivity.java
@@ -1,13 +1,33 @@
 package comsci.sanoman.animalforfun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.btnStrat);
+        final EditText editText= (EditText) findViewById(R.id.txtname);
+        button.setOnClickListener(new View.OnClickListener() {//ให้พิมพ์แค่ new On
+            @Override
+            public void onClick(View view) {
+                if (editText.length()==0){ //ุถ้าedittext ว่างยังไม่กรอกชื่อ
+                    Toast.makeText(getApplication(),"กรุณาใส่ชื่อก่อน",Toast.LENGTH_SHORT).show();
+                }else {
+                    String nameString = editText.getText().toString().trim();
+                    Toast.makeText(getApplicationContext(), "Hello "+nameString, Toast.LENGTH_SHORT).show();
+                    Intent startIntent=new Intent(MainActivity.this,Game.class);
+                    startActivity(startIntent);
+                }
+            }});
     }
 }
